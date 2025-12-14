9743978b234bb1f87a16e78f2a61bd4d2b91f107
Ksenia Latvis <ksenia.latvis@gmail.com>
2025-12-14 14:55:19 +0000

    Update start_caddy.sh

--- scripts/start_caddy.sh
+++ scripts/start_caddy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-exec caddy run --config Caddyfile --adapter caddyfile 2>&1/Users/ksenia/Downloads/browserless-v2-main/scripts/start_caddy.sh
+exec caddy run --config Caddyfile --adapter caddyfile 2>&1
